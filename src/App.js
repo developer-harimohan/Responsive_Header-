bdc74ef758cd834d753f0dc92b4ea2572bdb5d3b
developer-harimohan <harimohan.dev.05@gmail.com>
2023-01-05 11:38:35 +0530

    delete navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 function App() {
